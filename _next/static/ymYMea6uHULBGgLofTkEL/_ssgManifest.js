ef95f259e4b3986b964020a9f716c335920ad7f7
二维是熊 <a30232069383@gmail.com>
2026-02-09 18:46:26 +0800

    Fix: Use relative paths for assets

---
+++ _next/static/ymYMea6uHULBGgLofTkEL/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fability\u002F[id]","\u002Fability\u002F[id]\u002Fedit","\u002Fnews\u002F[id]","\u002Fnews\u002F[id]\u002Fedit"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
